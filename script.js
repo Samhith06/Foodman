976817749cd1c833032979d2650bfcea55c8cb5e
Samhith06 <samhith06@gmail.com>
2025-07-25 00:04:01 +0530

    completed a The resposive navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbar =document.getElementById('navbar');
+
+function opensidebar() {
+    navbar.classList.add('show');
+}
+function closesidebar() {
+    navbar.classList.remove('show');
+}
